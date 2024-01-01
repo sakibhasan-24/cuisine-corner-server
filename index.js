042931ff49ba3d673895a4c524b45e11f59dec80
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-01-02 02:10:04 +0600

    post request for order items

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 // Cuisine-corner
 //XeGWq4kq63zB4wZy
 app.use(cors());
+app.use(express.json());
 const uri = process.env.MONGO_URI;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +27,7 @@ async function run() {
     client.connect();
     const menuDatabase = client.db("Cuisine-corner").collection("Menu");
     const reviesCollection = client.db("Cuisine-corner").collection("Review");
+    const itemsCollection = client.db("Cuisine-corner").collection("Items");
     app.get("/menu", async (req, res) => {
       const data = await menuDatabase.find().toArray();
       res.send(data);
@@ -33,6 +35,14 @@ async function run() {
     app.get("/review", async (req, res) => {
       const data = await reviesCollection.find().toArray();
       res.send(data);
+    });
+    app.post("/items", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await itemsCollection.insertOne(data);
+      res
+        .status(201)
+        .send({ message: "successfully added", result, success: true });
     });
   } finally {
     console.log("yes!!!!");
